0e9e4f29feed428a2508249a57dcfbf46ea9a5dc
Desk1 <cherianjacob2014@gmail.com>
2023-03-12 19:23:10 +0000

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -11,6 +11,9 @@
 // @require      https://raw.githubusercontent.com/Desk1/ScoutUI/master/dist/script.js
 // @resource style https://raw.githubusercontent.com/Desk1/ScoutUI/master/dist/style.css
 
+// @updateURL    https://github.com/Desk1/ScoutUI/raw/master/userscript.user.js
+// @downloadURL  https://github.com/Desk1/ScoutUI/raw/master/userscript.user.js
+
 // ==/UserScript==
 
 GM_addStyle(GM_getResourceText('style'));
