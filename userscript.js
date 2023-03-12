41e916375dc180c63da0c9d2cde4c462b476cc50
Desk1 <cherianjacob2014@gmail.com>
2023-03-12 19:08:36 +0000

    guh

--- userscript.js
+++ userscript.js
@@ -2,14 +2,14 @@
 // @name         Scout UI
 // @version      0.1
 // @description  Custom hordes.io UI
-// @author       Desk
+// @author       Bau
 // @match        https://hordes.io/play
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
 
-// @require      file://C:\Users\Ken Jacob\Documents\HordesPvP\hordes\scoutUI\dist\bundle.js
-// @resource style file://C:\Users\Ken Jacob\Documents\HordesPvP\hordes\scoutUI\src\css\style.css
+// @require      https://raw.githubusercontent.com/Desk1/ScoutUI/master/dist/script.js
+// @resource style https://raw.githubusercontent.com/Desk1/ScoutUI/master/dist/style.css
 
 // ==/UserScript==
 
